28afda949161b5c345aa1d8f39d9d7cf4bf755f7
Valentin <fomkin.valya@mail.ru>
2022-07-24 15:45:46 +0300

    first commit

---
+++ udemy.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*Обучение с UDEMY */
+
+// let storeName = "Apple";
+// let storeDescription = {
+//   budget: 10000,
+//   employees: ["Elena", "Anna", "Vadim"],
+//   products: {
+//     bread: 10,
+//     water: 12,
+//   },
+//   open: true,
+// };
+
+/* 13. Простое общение с пользователем 
+
+все команды ниже нужно тестировать только в браузере, так как vs code их не знает
+ и будет выдавать ошибку при их проверке в программе*/
+
+// alert("Hello");
+
+// const resuls = confirm("are you here?");
+// console.log(resuls);
+
+// const answer = prompt("Вам есть 18?", "Да, мне есть 18");
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?");
+// answers[1] = prompt("Как ваша фамилия?");
+// answers[2] = prompt("Сколлько вам лет?");
+
+/* 14. Интерполяция 
+Знак $
+*/
+
+// const category = "toys";
+
+// console.log("https://someurl.com/" + category + "/" + "4");
+// /*Чтобы так не писать, можно использовать знак $ вместе с обратными кавычками ``
+//  */
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}!`);
+
+/*15. Операторы js
+ */
